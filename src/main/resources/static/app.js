dfea1bb404e6fc88145fae43f30565a0c8a529bf
= <benjamin.michael774@myci.csuci.edu>
2024-03-24 12:40:52 -0700

    fixed UUID generation bug causing server to not work

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -113,8 +113,16 @@ class Player {
     constructor(name) {
         this.name = name;
         this.ready = false;
-        this.id = crypto.randomUUID();
+        this.id = generateUUID();
     }
+}
+
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
 
 var player;
